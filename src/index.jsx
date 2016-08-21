b8e4c0d95740e107e57b21910e1e6a3bab19b795
Lucas Woo <legendecas@gmail.com>
2016-08-21 14:39:19 +0800

    project.init()

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'materialize-css/dist/js/materialize.min';
+import 'materialize-css/dist/css/materialize.min.css';
+
+ReactDOM.render(
+  <div>Hello world!</div>,
+  document.getElementById('content')
+);
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+/* eslint-disable strict, import/no-extraneous-dependencies */
+'use strict';
+
+const webpack = require('webpack');
+const path = require('path');
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+
+let plugins = [
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    'window.$': 'jquery',
+    'window.jQuery': 'jquery',
+    Hammer: 'hammerjs',
+  }),
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify(nodeEnv),
+    },
+  }),
+];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins = plugins.concat(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      comments: false,
+      sourceMap: false,
+      mangle: true,
+      minimize: true,
+    })
+  );
+} else {
+  plugins = plugins.concat([
+    function watchTimestamp() {
+      this.plugin('watch-run', (watching, callback) => {
+        /* eslint-disable no-console */
+        console.log(`Begin compile at ${new Date()}`);
+        callback();
+      });
+    },
+  ]);
+}
+
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    index: ['./src/index.jsx'],
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: 'dist/',
+    filename: 'js/[name].bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
+      {
+        test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file-loader?name=font/[name].[hash].[ext]',
+      },
+    ],
+  },
+  plugins,
+};
