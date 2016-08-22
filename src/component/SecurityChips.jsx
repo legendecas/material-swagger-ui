d3e73d1c1746a62a2d311038d96dde5941a1643c
Lucas Woo <legendecas@gmail.com>
2016-08-22 22:21:27 +0800

    feat: security chips tooltips

--- src/component/SecurityChips.jsx
+++ src/component/SecurityChips.jsx
@@ -1,17 +1,39 @@
 import React, { PropTypes } from 'react';
 import _ from 'lodash';
 
-const SecurityChips = ({ security }) => (
-  <div className="entrypoint-securities">
-    {_.flatMap(security, securities =>
-      _.flatMap(securities, (scopes, name) =>
-        _.map(scopes, scope =>
-          <div key={name + scope} className="chip">
-            {name}:{scope}
-          </div>
-        )))}
-  </div>
-);
+const SecurityChips = ({ security }) => {
+  const object = {};
+  const list = _.flatMap(security, securities =>
+    _.flatMap(securities, (scopes, name) =>
+      _.map(scopes, scope => ({ scope, name }))));
+  list.forEach(({ scope, name }) => {
+    if (object[scope]) {
+      object[scope] = object[scope].concat(name);
+    } else {
+      object[scope] = [name];
+    }
+  });
+
+  return (
+    <div className="entrypoint-securities">
+      {Object.getOwnPropertyNames(object).map(scope => {
+        const names = object[scope];
+        return (
+          <div
+            key={scope}
+            className="chip tooltipped"
+            data-position="bottom"
+            data-delay="50"
+            data-tooltip={names.join(', ')}
+            ref={ref => $(document).ready(() => {
+              $(ref).tooltip({ delay: 50 });
+            })}
+          >{scope}</div>
+        );
+      })}
+    </div>
+  );
+};
 
 SecurityChips.propTypes = {
   security: PropTypes.arrayOf(PropTypes.object).isRequired,
