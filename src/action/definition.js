d04bc5a97f37e3ac85dbe722405b39394939447c
Lucas Woo <legendecas@gmail.com>
2016-08-25 20:50:18 +0800

    feat: simple yaml support

--- src/action/definition.js
+++ src/action/definition.js
@@ -1,8 +1,15 @@
 import request from 'superagent';
+import yaml from 'js-yaml';
 
 const action = {
   getDefinition: url => dispatch => {
-    request.get(url).then(response => dispatch(action.gotDefinition(response.body)));
+    request.get(url).then(response => {
+      if (url.endsWith('yaml') || url.endsWith('yml')) {
+        dispatch(action.gotDefinition(yaml.safeLoad(response.text)));
+      } else {
+        dispatch(action.gotDefinition(response.body));
+      }
+    });
   },
   gotDefinition: definition => ({
     type: 'GOT_DEFINITION',
