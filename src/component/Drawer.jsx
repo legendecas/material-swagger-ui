e8dd28a1aa8121c09a31e34d46ad5a1954a2d0a9
Lucas Woo <legendecas@gmail.com>
2016-09-30 15:21:50 +0800

    fix: drawer anchor item effect

--- src/component/Drawer.jsx
+++ src/component/Drawer.jsx
@@ -16,16 +16,29 @@ const Drawer = ({ tags }) => (
       const offset = $(ref).offset() || {};
       const top = offset.top;
       $(ref).pushpin({ top });
+
+      $(window).scroll(() => {
+        $('.side-nav li').removeClass('active');
+        $('.side-nav li .collapsible-body').css('display', 'none');
+        $('.side-nav li .collapsible-body ul li a').parent().removeClass('active');
+
+        const activeAnchor = $('.side-nav li .collapsible-body ul li .active').first();
+        activeAnchor.parent().parent().parent().parent()
+          .addClass('active');
+        activeAnchor.parent().parent().parent().css('display', 'block');
+        activeAnchor.parent().addClass('active');
+      });
     }}
     id="slide-out"
-    className={`side-nav fixed collapsible collapsible-accordion ${css(style.taglist)}`}
+    className={`side-nav fixed collapsible ${css(style.taglist)}`}
+    data-collapsible="accordion"
   >
     <li>
       <a className="waves-effect waves-teal" href="#api-description">Api Description</a>
     </li>
     {tags.map(tag =>
       <li key={tag.name}>
-        <a className="collapsible-header waves-effect waves-teal" href={`#${tag.name}`}>
+        <a className="collapsible-header waves-effect waves-light" href={`#${tag.name}`}>
           {tag.name}
           <span className="grey-text">{_.truncate(tag.description)}</span>
         </a>
@@ -35,10 +48,8 @@ const Drawer = ({ tags }) => (
               <li key={entrypoint.method + entrypoint.path}>
                 <a
                   href={`#${_.kebabCase(entrypoint.method + entrypoint.path)}`}
-                  className="waves-effect waves-teal"
-                >
-                  {entrypoint.method.toUpperCase()} {entrypoint.path}
-                </a>
+                  className="waves-effect waves-light truncate"
+                >{entrypoint.method.toUpperCase()} {entrypoint.path}</a>
               </li>
             )}
           </ul>
